1a19a8933263323bdeb6a22a035ecc927448c79d
Dafabotz <115438942+Dafabotz@users.noreply.github.com>
2022-11-04 17:27:51 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -26,24 +26,24 @@ global.language = indonesia  //change indonesia to english if you don't understa
 
 // Other
 // Other
-global.botname = "Alphabot-Mdོ" //namabot kalian
-global.ownername= "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //nama kalian
+global.botname = "DAFFA-BOT-MD" //namabot kalian
+global.ownername= "ᴹᴿ᭄ DaffaXD ×፝֟͜×" //nama kalian
 global.myweb ="https://api.zeeoneofc.xyz" //bebas asal jan hapus
-global.youtube = "https://youtube.com/c/ZeeoneOfc" //bebas asal jan hapus
+global.youtube = "https://youtube.com/c/DaffaXD REAL" //bebas asal jan hapus
 global.myweb2 = "https://linktr.ee/zeeoneofc" //bebas
-global.email = "zeeoneofc@gmail.com" //bebas
+global.email = "dafastore1245@gmail.com" //bebas
 global.region = "Indonesia" //bebas
 global.timezone = 'Asia/Jakarta' //  timezone wib
-global.premium = ['62887435047326'] //premium user
-global.owner = ["6285342106390","622150996855"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "62887435047326" // nomor wa kalian
-global.ownernomerr = "+62887435047326" //nmr wa kalian
+global.premium = ['6288271109247'] //premium user
+global.owner = ["6288271109247","6288708018815"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "6288271109247" // nomor wa kalian
+global.ownernomerr = "+6288708018815" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
-global.donasi = "./image/donasi.jpg" // foto donasi di folder image
-global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
+global.donasi = "./https://telegra.ph/file/860383decf5821a5038a3.jpg" // foto donasi di folder image
+global.background_welcome="https://https://telegra.ph/file/af69eb0c1db3d0a93a640.jpg" // maks size 30kb, agar welcome image nya tdk delay
 global.thumbGif = 'https://telegra.ph/file/e90bef3779d3c5812a5d1.mp4'
-global.packname = '© Alphabot-Mdོ' //sticker wm ubah
-global.author = 'Di Buat Oleh ZeeoneOfc' //sticker wm ganti nama kalian
+global.packname = '© DAFFA BOT' //sticker wm ubah
+global.author = 'Di Buat Oleh ABANG DAPA' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.typemenu = 'document'
 global.multiplier = 69
@@ -70,18 +70,18 @@ global.rpg = {
   emoticon(string) {
     string = string.toLowerCase()
     let emot = {
-      level: '📊',
-      limit: '🎫',
-      health: '❤️',
-      exp: '✨',
-      money: '💹',
-      bank: '🏦',
-      potion: '🥤',
-      diamond: '💎',
-      common: '📦',
-      uncommon: '🛍️',
-      mythic: '🎁',
-      legendary: '🗃️',
+      level: '💪',
+      limit: '🗿',
+      health: '🤣',
+      exp: '👊',
+      money: '🚭',
+      bank: '😑',
+      potion: '🔅',
+      diamond: '😯',
+      common: '🖕',
+      uncommon: '🥶',
+      mythic: '🖕',
+      legendary: '🗃💀',
       superior: '💼',
       pet: '🔖',
       trash: '🗑',
@@ -120,7 +120,7 @@ global.mess = {
     sukses: '🤗Done, Oke Desu~',
     admin: 'Perintah Ini Hanya Bisa Digunakan Oleh Admin !',
     botAdmin: 'Perintah Ini Hanya Bisa Digunakan Ketika Bot Menjadi Admin !',
-    owner: 'Perintah Ini Hanya Bisa Digunakan Oleh Owner !',
+    owner: 'Perintah Ini Hanya Bisa Digunakan Oleh Owner ganteng !',
     group: 'Perintah Ini Hanya Bisa Digunakan Di Group !',
     private: 'Perintah Ini Hanya Bisa Digunakan Di Private !',
 	bot: 'Fitur Khusus Pengguna Nomor Bot',
